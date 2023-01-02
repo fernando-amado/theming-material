83976e6f9c49ceaf52551ae4325c65bd1f8d0dd2
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-01-02 10:09:17 -0500

    Creacion de interface para modificacion de directiva para componentes

--- angular-material/src/app/Directivas/size.directive.ts
+++ angular-material/src/app/Directivas/size.directive.ts
@@ -1,157 +1,47 @@
 import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
+import { Component } from '../interfaces/Component';
 @Directive({
   selector: '[size]',
 })
-export class SizeDirective {
-  constructor(private el: ElementRef) {
-    el.nativeElement.style.customProperty = true;
-  }
+export class SizeDirective implements Component {
+  constructor(private el: ElementRef) {}
+  button: string = 'button';
+  matChip: string = 'mat-chip';
+  matChipOption: string = 'mat-chip-option';
+  matFormField: string = 'mat-form-field';
+  matCheckBox: string = 'mat-checkbox';
+  matSlider: string = 'mat-slider';
+  matRadioButton: string = 'mat-radio-button';
+  matSpinner: string = 'mat-spinner';
+  table: string = 'table';
+  matTable: string = 'mat-table';
+  matIcon: string = 'mat-icon';
+
   @Input() size!: string;
   ngOnChanges() {
-    let typeElement = this.el.nativeElement.localName;
-    function SetStyle(
-      element: {
-        style: {
-          height: string;
-          paddingInline: string;
-          fontSize: string;
-          padding: string;
-          width: string;
-        };
-        classList: { add: (arg0: string) => void };
-      },
-      heightCase: string,
-      fontSizeCase: string,
-      paddingInlineCase: string,
-      classCase: string,
-      paddingCase: string,
-      widthCase: string
-    ) {
-      if (heightCase) {
-        element.style.height = heightCase;
-      }
-      if (paddingInlineCase) {
-        element.style.paddingInline = paddingInlineCase;
-      }
-      if (fontSizeCase) {
-        element.style.fontSize = fontSizeCase;
-      }
-      if (classCase) {
-        element.classList.add(classCase);
-      }
-      if (paddingCase) {
-        element.style.padding = paddingCase;
-      }
-      if (widthCase) {
-        element.style.width = widthCase;
-      }
-    }
+    let element = this.el.nativeElement;
     switch (this.size) {
       case 'small':
-        if (typeElement === 'mat-chip') {
-          SetStyle(
-            this.el.nativeElement,
-            '24px',
-            '13px',
-            '8px',
-            'undefined',
-            'undefined',
-            'undefined'
-          );
-        }
-        if (typeElement === 'mat-chip-option') {
-          SetStyle(
-            this.el.nativeElement,
-            '24px',
-            '13px',
-            '8px',
-            'undefined',
-            'undefined',
-            'undefined'
-          );
-        } else if (typeElement === 'button') {
-          SetStyle(
-            this.el.nativeElement,
-            '32px',
-            '13px',
-            'undefined',
-            'undefined',
-            'undefined',
-            'undefined'
-          );
-        } else if (typeElement === 'mat-form-field') {
-          SetStyle(
-            this.el.nativeElement,
-            '70px',
-            '16px',
-            'undefined',
-            'small',
-            'undefined',
-            'undefined'
-          );
-        } else if (
-          typeElement === 'mat-checkbox' ||
-          typeElement === 'mat-slider' ||
-          typeElement === 'mat-radio-button' ||
-          typeElement === 'mat-spinner' ||
-          typeElement === 'table' ||
-          typeElement === 'mat-table'
-        ) {
-          SetStyle(
-            this.el.nativeElement,
-            'undefined',
-            'undefined',
-            'undefined',
-            'small',
-            'undefined',
-            'undefined'
-          );
-        } else if (typeElement === 'mat-icon') {
-          SetStyle(
-            this.el.nativeElement,
-            'undefined',
-            '20px',
-            'undefined',
-            'undefined',
-            'undefined',
-            'undefined'
-          );
+        {
+          this.button = element.classList.add('small');
+          this.matChip = element.classList.add('small');
+          this.matChipOption = element.classList.add('small');
+          this.matFormField = element.classList.add('small');
+          this.matCheckBox = element.classList.add('small');
+          this.matRadioButton = element.classList.add('small');
+          this.matSlider = element.classList.add('small');
+          this.matSpinner = element.classList.add('small');
+          this.table = element.classList.add('small');
+          this.matTable = element.classList.add('small');
+          this.matIcon = element.classList.add('small');
         }
         return;
       case 'large':
-        if (typeElement === 'button') {
-          SetStyle(
-            this.el.nativeElement,
-            'undefined',
-            '15px',
-            'udenfined',
-            'undefined',
-            '20px 18px',
-            'undefined'
-          );
-        } else if (
-          typeElement === 'mat-checkbox' ||
-          typeElement === 'mat-radio-button'
-        ) {
-          SetStyle(
-            this.el.nativeElement,
-            'undefined',
-            'undefined',
-            'undefined',
-            'large',
-            'undefined',
-            'undefined'
-          );
-        } else if (typeElement === 'mat-icon') {
-          SetStyle(
-            this.el.nativeElement,
-            '30px',
-            '35px',
-            'undefined',
-            'undefined',
-            'undefined',
-            '33px'
-          );
+        {
+          this.button = element.classList.add('large');
+          this.matCheckBox = element.classList.add('large');
+          this.matRadioButton = element.classList.add('large');
+          this.matIcon = element.classList.add('large');
         }
         return;
       default:
---
+++ angular-material/src/app/interfaces/Component.ts
@@ -0,0 +1,13 @@
+export interface Component {
+  button: string;
+  matChip: string;
+  matChipOption: string;
+  matFormField: string;
+  matCheckBox: string;
+  matSlider: string;
+  matRadioButton: string;
+  matSpinner: string;
+  table: string;
+  matTable: string;
+  matIcon: string;
+}
