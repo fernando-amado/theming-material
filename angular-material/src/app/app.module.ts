70e6e9ec03176d066d154365a7f49f3460240ed7
jaider toro <jaisembergll@gmail.com>
2023-01-04 16:33:54 -0500

    creacion de historia propia para la tipografia

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -4,18 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { TypographyComponent } from '../stories/typography/typography.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    TypographyComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
   ],
+  exports:[],
   providers: [],
   bootstrap: [AppComponent]
 })
