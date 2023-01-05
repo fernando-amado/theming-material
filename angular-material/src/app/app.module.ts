abce97d630239dc0f66697e11a3336bcb214faff
jaider toro <jaisembergll@gmail.com>
2023-01-05 08:43:40 -0500

    organizacion de importaciones de modulos y componentes en el app module parte 2

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
 import { AppComponent } from './app.component';
 import { TypographyComponent } from '../stories/typography/typography.component';
 
