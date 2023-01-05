143fca289bd6b4baa2953bc012d88863351a7692
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-01-05 16:56:51 -0500

    Revert "Revert "Agregando componentes en selector""
    This reverts commit 79ddd8a4dd14a90dd2a1eeeea2c2b099342bea12.

--- angular-material/src/app/Directivas/size/size.directive.ts
+++ angular-material/src/app/Directivas/size/size.directive.ts
@@ -3,7 +3,7 @@ import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 export type Size = 'small' | 'medium' | 'large';
 
 @Directive({
-  selector: `button,mat-form-field [size]`,
+  selector: `button,mat-form-field,mat-chip,mat-chip-option,mat-checkbox,mat-radio-button,mat-spinner,mat-slider,table,mat-table,mat-icon `,
 })
 export class SizeDirective implements OnChanges {
   constructor(private el: ElementRef<HTMLElement>) {}
