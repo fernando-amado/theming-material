61b74c3e8a3cc43996b063c2ac13998dc27c25b1
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-01-10 16:51:00 -0500

    button group adelanto

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TypographyComponent } from '../stories/typography/typography.component';
+import { ButtonGroupComponent } from '../stories/components/button-group/button-group.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TypographyComponent,
-  ],
+  declarations: [AppComponent, TypographyComponent, ButtonGroupComponent],
+  exports: [],
+  providers: [],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
   ],
-  exports:[],
-  providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- angular-material/src/stories/components/button-group/button-group.component.ts
+++ angular-material/src/stories/components/button-group/button-group.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+
+export type Colors = 'primary' | 'accent'| 'warn' | 'info' | 'warning' | 'success';
 
 @Component({
-  selector: 'app-button-group',
+  selector: 'button-group',
   templateUrl: './button-group.component.html',
-  styleUrls: ['./button-group.component.scss']
+  styleUrls: ['./button-group.component.scss'],
 })
-export class ButtonGroupComponent {
+export class ButtonGroupComponent implements OnInit {
+  constructor(private el: ElementRef<HTMLHtmlElement>) {}
+  @Input() color!: Colors
 
+  ngOnInit(): void {
+    console.log(this.el.nativeElement)
+
+    this.el.nativeElement.classList.add(`${this.color}-buttonGroup`)
+
+
+  }
 }
