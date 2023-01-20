445f3dcfcaf4399b2aa3c018597cb13f6288d5a7
Sebastian0916 <Sebas.vera0916@gmail.com>
2023-01-20 12:53:19 -0500

    Configracion checkbox nuevos tamaños

---
+++ angular-material/src/components/checkbox/checkbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['../..//styles/main.scss']
+
+})
+export class CheckboxComponent {
+
+}
