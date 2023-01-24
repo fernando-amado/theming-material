8cba0b3f1940f9d67b7450898f1ab93555e8b43a
David22zambrano <david.zambrano17@outlook.com>
2023-01-24 16:01:54 -0500

    Upload theming angular

--- angular-material/src/theme/components/mat-spinner/mat-spinner.component.ts
+++ angular-material/src/theme/components/mat-spinner/mat-spinner.component.ts
@@ -1,17 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Directive, Input } from '@angular/core';
+import { Size } from '../../directives/size-directive/size.directive';
 @Component({
   selector: 'app-mat-spinner',
   templateUrl: './mat-spinner.component.html',
-  styleUrls: ['../..//styles/main.scss'],
- providers:[ 
-],
-
 })
 export class MatSpinnerComponent {
   @Input() 
   color?: 'primary'|'accent'|'warn'|'success'|'info'|'warning' = 'primary' ;
-  
+  @Input()
+  size: Size = 'small';
   public get colorOption(): string[] {
-    return [`app-mat-spinner--${this.color}`]
+    return [`app-mat-spinner--${this.color}`,`${this.size}`]
   }
 }
