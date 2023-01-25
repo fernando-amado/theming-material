32e62ddbae5fd7fe5277dade1cbe31abf81ef184
David22zambrano <david.zambrano17@outlook.com>
2023-01-25 16:56:46 -0500

    Avance correcion medidas componentes

---
+++ angular-material/src/components/mat-tabs/mat-tabs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-tabs',
+  templateUrl: './mat-tabs.component.html',
+  styleUrls: ['../..//styles/main.scss']
+})
+export class MatTabsComponent {
+
+}
